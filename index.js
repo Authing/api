aeb7f03fb73938dc18545e7767859001b3dbbe28
liaochangjiang <changjiangthecoder@gmail.com>
2020-02-03 13:52:43 +0800

    Add API to dump api info

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const API = require("./api")
+const app = express()
+const port = 5678
+
+app.get('/list', function (req, res) {
+  res.send(API)
+})
+
+app.get('/api/:name', function (req, res) {
+  const name = req.params['name']
+  const api = API[name]
+  if (!api) {
+    res.send({
+      code: 404,
+      message: `API <${name}> not found.`
+    })
+    return
+  }
+
+  res.send(api)
+})
+
+app.listen(port, () => console.log(`Example app listening on http://localhost:${port} 🚀!`))
