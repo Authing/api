059d5d00d4b90ae5ef7852c466d4ce97af42394c
liaochangjiang <changjiangthecoder@gmail.com>
2020-02-04 10:13:21 +0800

    Add doc for deleteRBACGroupBatch

--- api/deleteRBACGroupBatch/index.js
+++ api/deleteRBACGroupBatch/index.js
@@ -8,7 +8,8 @@ const query = fs.readFileSync(path.join(__dirname, 'query.gql'), 'utf8')
 module.exports = {
   name: 'deleteRBACGroupBatch',
   type: 'mutation',
-  description: '',
+  module: '权限控制',
+  description: '批量删除分组',
   query,
   doc
 }
