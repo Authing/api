a64e4df6bbd371df85fb0d3a7ad491b63d9289f4
liaochangjiang <changjiangthecoder@gmail.com>
2020-02-04 10:09:22 +0800

    Add doc for updateRBACGroup

--- api/updateRBACGroup/index.js
+++ api/updateRBACGroup/index.js
@@ -8,7 +8,8 @@ const query = fs.readFileSync(path.join(__dirname, 'query.gql'), 'utf8')
 module.exports = {
   name: 'updateRBACGroup',
   type: 'mutation',
-  description: '',
+  module: '权限控制',
+  description: '更新分组',
   query,
   doc
 }
